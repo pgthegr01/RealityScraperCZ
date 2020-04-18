afc57627f2409bb01d7f419c6f6b4c63c0e6ef34
paul <paul_ginnane@hotmail.com>
2020-04-18 23:42:09 +0200

    changing to restart

--- RealityScraperCZ.sh
+++ RealityScraperCZ.sh
@@ -4,5 +4,5 @@ source $HOME/github/RealityScraperCZ_env/bin/activate
 PATH=$PATH:/usr/local/bin
 export PATH
 cd $HOME/github/RealityScraperCZ
-docker start "80a9b83ac2aec74c8aacec9d29dc646cbf5b6d0dc5655ee7e1cece9223a6bba1"
+docker restart "80a9b83ac2aec74c8aacec9d29dc646cbf5b6d0dc5655ee7e1cece9223a6bba1"
 scrapy crawl sreality
